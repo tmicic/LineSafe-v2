5b62c012eb77dc7ee2e9b899578fe9d8b745e9e5
Thomas Micic <thomasmicic@gmail.com>
2021-04-30 16:47:46 +0100

    implemention begin of LineSafe Dataset

--- custom_datasets.py
+++ custom_datasets.py
@@ -166,17 +166,43 @@ class UnetDataset(UnbalancedDataset):
 class LineSafeDataset():
 
     def __init__(self, 
-                    df_path,
-                    dicom_root,
-                    segmentation_map_root=None,
-                    transform=None,
-                    return_type='bal',
-                    loader=dicom_processing.auto_loader,
-                    target_loader=dicom_processing.segmentation_image_loader,
-                    default_blank_seg_map_size=(256,256),
-                    seg_map_transform=None):
+                    df_path,    # path the csv file
+                    dicom_root, # path the dicom images
+                    segmentation_map_root=None, # path to the ng tube segmentation maps
+                    transform=None,     # transform applied to the dicom_image
+                    ignore_no_ng=True,  # removes no_ngs from the dataset
+                    loader=dicom_processing.auto_loader,    # loader used for the dicom images
+                    target_loader=dicom_processing.segmentation_image_loader,   # loader used for the segmentation maps
+                    default_blank_seg_map_size=(256,256),   # if no segmentation map, default blank image size to return
+                    seg_map_transform=None,  # transform applied to the segmentation map 
+                    only_images_with_segmentations=False, # if true, only images with segmentation maps will be returned, if false, if no segmentation map, blank maps are returned                
+                    return_type=None,# see below
+                    random_v_flip_rate=None,    # rate of how often a v flip is done, 
+                    random_h_flip_rate=None,    # rate of how often a h flip is done - if none, no flipping
+                    random_4_point_rotation_rate=None, # list of frequencies of rotations or 0, 90, 180, and 270
+                    return_segmentations=True
+                    ):
 
+                    # return_type 
+                    #   None or 'unbal' = unbalanced (dicom, category, [segmentation])
+                    #   'bal' = balanced (dicom, category, [segmentation])  
+                    #   'contrastive' = (dicom1, category1, [segmentation1], dicom2, category2, [segmentation2], is_diff)
+                    #   'triplet' = (dicom_anchor, category_anchor, [segmentation_anchor], positive_dicom, 
+                    #               positive_category, [positive_segmentation], negative_dicom, negative_cat, [negative_segmentation])
+        
         self.df_path = df_path
+        self.dicom_root = dicom_root
+        self.seg_map_root = segmentation_map_root
+        self.random_v_flip_rate = random_v_flip_rate
+        self.random_h_flip_rate = random_h_flip_rate
+        self.random_4_point_rotation_rate=random_4_point_rotation_rate,
+        self.transform = transform
+        self.seg_map_transform = seg_map_transform
+        self._default_map_size = default_blank_seg_map_size
+        self.loader = loader
+        self.target_loader = target_loader
+        self.return_segmentations = return_segmentations
+        self.return_type = return_type
 
         if df_path.endswith('.csv'):
             self.df = pd.read_csv(df_path, index_col=0)
@@ -184,30 +210,63 @@ class LineSafeDataset():
             self.df = pd.read_feather(df_path)
             del self.df['index']
 
-        self.loader = loader
-        self.target_loader = target_loader
+        if only_images_with_segmentations:
+            self.df = self.df[self.df['ng_roi_filename'].notnull()]
+
+        if ignore_no_ng:
+            self.df = self.df[self.df['category'] != 'NO_NG']
 
         self.classes = sorted(self.df['category'].unique())                 # make sure that training and validation set have same classes!!! in same order!!
-        self.class_to_id = {x:i for i,x in enumerate(self.classes)}
+        self.class_to_id = {x:i for i,x in enumerate(self.classes)}        
+        
+        if return_type == 'bal':
+            min_count = min(self.df.groupby('category')['hash'].count().values)
+            self.df = pd.concat([self.df[self.df['category']==c].sample(n=min_count) for c in self.classes])
 
-        self.transform = transform
-        self.seg_map_transform = seg_map_transform
-
-
+    def __len__(self) -> int:
+        return len(self.df)
 
 
     def __getitem__(self, index: int):
         
         item = self.df.iloc[index]
 
-        X = self.loader(os.path.join(self.root, item['category'],item['filename']))
+        X = self.loader(os.path.join(self.dicom_root, item['category'], item['filename']))
         if self.transform is not None: X = self.transform(X)
-            
-        y = torch.tensor(self.class_to_id[item['category']])
-        
-        if self.target_transform is not None: y = self.target_transform(y)
 
-        return X, y
+        y = torch.tensor(self.class_to_id[item['category']])
+
+        seg = None
+        if self.return_segmentations:
+            if item['ng_roi_filename'] is None:
+            # return blank target
+                seg = np.zeros(self.blank_map_dims)
+            else:
+                seg = self.target_loader(os.path.join(self.map_root,item['ng_roi_filename']))
+
+        if seg is not None and self.seg_map_transform is not None:
+            seg = self.seg_map_transform(seg)
+
+        if self.return_type is None or self.return_type == 'bal' or self.return_type == 'unbal':
+            if seg is None:
+                return X, y
+            else:
+                return X, y, seg
+        elif self.return_type=='contrastive':
+            raise NotImplementedError()
+        elif self.return_type=='triplet':
+            raise NotImplementedError()
+        else:
+            raise NotImplementedError() 
+
+
+
+
+
+        
+        # if self.target_transform is not None: y = self.target_transform(y)
+
+        # return X, y
 
 
 
@@ -279,27 +338,45 @@ class SiameseDataset(UnbalancedDataset):
 
 
 if __name__ == '__main__':
-    print('debugging')
-    import custom_transforms
 
-    transform = transforms.Compose([
-                                
-                                transforms.ToTensor(),
-                                custom_transforms.ToMultiChannel(3),
-                                transforms.Resize((256,256)),
-                            ])
+    DATASET_PATH = r'datasets\train.csv'
+    SATO_IMAGES_ROOT_PATH = r'C:\Users\Tom\Google Drive\Documents\PYTHON PROGRAMMING\AI\data\SATO1'
+    NG_ROI_ROOT_PATH = r'C:\Users\Tom\Google Drive\Documents\PYTHON PROGRAMMING\AI\data\ROIS\NG_ROI'
+    
+    dataset = LineSafeDataset(df_path=DATASET_PATH, 
+                            dicom_root=SATO_IMAGES_ROOT_PATH, 
+                            segmentation_map_root=NG_ROI_ROOT_PATH,
+                            ignore_no_ng=False,
+                            only_images_with_segmentations=False,
+                            return_type='bal')
+
+    print(dataset.df)
 
     
-    train_dataset = SiameseDataset(common.TRAIN_DF_PATH, 
-                            root=common.SATO_IMAGES_ROOT_PATH, 
-                            loader=dicom_processing.auto_loader,
-                            transform=transform,
-                            target_transform=None,
-                            ignore_no_tubes=True, 
-                            return_type='contrastive')
 
 
-    train_dataset.__getitem__(0)
+
+    # import custom_transforms
+
+    # transform = transforms.Compose([
+                                
+    #                             transforms.ToTensor(),
+    #                             custom_transforms.ToMultiChannel(3),
+    #                             transforms.Resize((256,256)),
+    #                         ])
+
+    
+    # train_dataset = LineSafeDataset(common.TRAIN_DF_PATH,
+    #                         dicom_root=common. 
+    #                         root=common.SATO_IMAGES_ROOT_PATH, 
+    #                         loader=dicom_processing.auto_loader,
+    #                         transform=transform,
+    #                         target_transform=None,
+    #                         ignore_no_tubes=True, 
+    #                         return_type='contrastive')
+
+
+    # train_dataset.__getitem__(0)
 
 
 
